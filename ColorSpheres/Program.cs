189917048f5466b21bdc2397abb3a06ae6e954ed
Dcodigo1Max <davidrasinhas.info@gmail.com>
2025-04-03 23:35:14 +0100

    Begin of ColorSpheres

--- ColorSpheres/Program.cs
+++ ColorSpheres/Program.cs
@@ -1,12 +1,54 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ColorSpheres
 {
     public class Program
     {
-        private static void Main(string[] args)
+        private byte RED = 50;
+        private byte GREEN =100;
+        private byte BLUE = 120;
+        private byte Alpha;
+
+        public Program(byte RED, byte BLUE, byte GREEN, byte Alpha)
         {
-            Console.WriteLine("Hello LP1!");
+            this.RED=RED;
+            RED =Convert.ToByte(255);
+            this.BLUE=BLUE;
+            this.GREEN=GREEN;
+            this.Alpha=Alpha;
+
+        }   
+
+        private static void Main(string[] args)
+        {           
+            
+            
+           
         }
-    }
+    
+
+        public byte GetRED()
+        {
+            return RED;
+        }
+
+        public byte GetBLUE()
+        {
+            return BLUE;
+        }
+
+        public byte GetGREEN()
+        {
+            return GREEN;
+        }
+
+        public byte GetAlpha()
+        {
+            return Alpha;
+        }
+
+
+
+    }  
 }
