811abb554efff4e926a3fb535fddeab7835fc309
Dcodigo1Max <davidrasinhas.info@gmail.com>
2025-04-04 15:44:11 +0100

    New Class Colors

---
+++ BetterColorSpheres/Color.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace BetterColorSpheres
+{
+    public class Color
+    {
+        private string name;
+        private byte red = 50;
+        private byte green = 100;
+        private byte blue = 200;
+        private byte alpha = 255;
+        private byte grey;
+
+
+                public byte GetRED()
+        {
+            return red;
+        }
+
+        public byte GetBLUE()
+        {
+            return blue;
+        }
+
+        public byte GetGREEN()
+        {
+            return green;
+        }
+
+        public byte GetAlpha()
+        {
+            return alpha;
+        }
+
+
+        public byte Grey => grey = Convert.ToByte((red + blue + green)/3);
+            
+
+        
+    
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+        }
+    }
+}
