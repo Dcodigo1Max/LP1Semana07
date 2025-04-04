850311577b18119ef4117680c3af693932736f28
Dcodigo1Max <davidrasinhas.info@gmail.com>
2025-04-04 15:26:53 +0100

    Added player.cs

---
+++ PlayerStats/Player.cs
@@ -0,0 +1,80 @@
+using System;
+
+namespace PlayerStats
+{
+    public class Player 
+    {
+        public float highScore {get; private set;}
+        public int playedGames {get; private set;}
+
+        public int wonGames {get; private set;}
+
+
+        public Player(string name)
+        {
+            Name = name;
+            highScore = 0;
+            playedGames = 0;
+            wonGames = 0;
+        }
+
+
+        public float HighScore
+        {
+            get
+            { 
+                return highScore; 
+            }
+            set
+            {
+                if (value > highScore)
+                {
+                    highScore = value;
+                }
+            }
+        }
+
+
+        public string Name {get;}
+
+        public float WinRate
+        {
+            get 
+            {
+                return (float) wonGames / playedGames; 
+            }
+            set
+            {
+                if (playedGames == 0)
+                WinRate = 0;
+            }
+        }
+
+
+        public void PlayGame(bool win)
+        {
+            playedGames++;
+            if(win)
+            {
+                wonGames++;
+            }
+        }
+
+
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+        }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+}
