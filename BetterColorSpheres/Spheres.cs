d4a4126516e24b7aeaf0a2ec2db94c43b2fbbf35
Dcodigo1Max <davidrasinhas.info@gmail.com>
2025-04-04 15:46:54 +0100

    Changes to Sphere

--- BetterColorSpheres/Spheres.cs
+++ BetterColorSpheres/Spheres.cs
@@ -2,70 +2,7 @@ using System;
 
 namespace ColorSpheres
 {
-    public class Color
-    {
-        private string name;
-        private byte red = 50;
-        private byte green = 100;
-        private byte blue = 200;
-        private byte alpha = 255;
-        private byte grey;
-
-
-       /* public ParaColor(string name)
-        {
-            this.name=name;
-            
-        }*/
-
-        public Color(byte red, byte blue, byte green, byte alpha)
-        {
-            this.red=red;
-            this.blue=blue;
-            this.green=green;
-            this.alpha=alpha;
-
-        }   
-
-
-
-        private static void Main(string[] args)
-        {  
-                 
-           //Console.ReadLine(Convert.ToString(red));
-           
-           
-           
-        }
     
-
-        public byte GetRED()
-        {
-            return red;
-        }
-
-        public byte GetBLUE()
-        {
-            return blue;
-        }
-
-        public byte GetGREEN()
-        {
-            return green;
-        }
-
-        public byte GetAlpha()
-        {
-            return alpha;
-        }
-
-
-        public byte Grey => grey = Convert.ToByte((red + blue + green)/3);
-            
-
-        
-    } 
-
     public class Spheres
     {
         private string cor;
